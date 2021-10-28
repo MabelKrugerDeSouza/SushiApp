db2b7c8aee420fe529c48f20f5079011f6b3f906
Mabel <mabelksouza@gmail.com>
2021-10-28 12:48:10 -0300

    Initial Commit

---
+++ Sushi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sushi
+//
+//  Created by Mabel Kruger de Souza on 28/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sushi/SushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiApp.swift
+//  Sushi
+//
+//  Created by Mabel Kruger de Souza on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
