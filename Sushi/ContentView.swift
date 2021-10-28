dac589028283a58518338f30d4346a34b00bd2e4
Mabel <mabelksouza@gmail.com>
2021-10-28 13:36:03 -0300

    criando a interface do app

--- Sushi/ContentView.swift
+++ Sushi/ContentView.swift
@@ -8,12 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    var body: some View{
+        VStack{
+            Header()
+            Categoria()
+            
+    }
+}
+struct Header: View {
+    var body: some View{
+        HStack{
+            Text("Discover")
+                .font(.system(size: 44, weight: .bold, design: .rounded))
+                .padding(.leading, 30)
+            Spacer()
+            
+            Image(systemName: "magnifyingglass")
+                .font(.system(size: 28, weight: .light))
+                .padding(.trailing, 30)
+        }
     }
 }
 
+struct Categoria: View{
+    var body: some View{
+        HStack(spacing: 50){
+            VStack{
+                Text("Popular")
+                    .font(.system(size: 16, weight: .bold, design: .rounded))
+                    .foregroundColor(Color.orange)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 20, height: 6)
+                    .foregroundColor(Color.orange)
+                    .padding(.top, -3)
+            }
+            VStack{
+                Text("Rating")
+                    .font(.system(size: 16, weight: .medium, design: .rounded))
+                    .foregroundColor(Color.gray)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 20, height:6)
+                    .foregroundColor(Color.orange)
+                    .opacity(0)
+                    .padding(.top, -3)
+            }
+            VStack{
+                Text("Recent")
+                    .font(.system(size: 16, weight: .medium, design: .rounded))
+                    .foregroundColor(Color.gray)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 20, height: 6)
+                    .foregroundColor(Color.orange)
+                    .opacity(0)
+                    .padding(.top, -3)
+            }
+            Spacer()
+        }
+        .padding(.leading, 30)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
